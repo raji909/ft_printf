4c4c7d05a915f898b93fe8e61dc124d27e81e0a5
Adam Raji <adraji@e1r12p5.1337.ma>
2025-10-28 22:54:20 +0100

    finish mandatory

--- ft_printf.c
+++ ft_printf.c
@@ -1,5 +1,24 @@
 #include "ft_printf.h"
 
+static int	ft_switch(va_list args, char fms)
+{
+	if (fms == 'd' || fms == 'i')
+		return (ft_putnbr(va_arg(args, int)));
+	else if (fms == 'u')
+		return (ft_putunbr(va_arg(args, unsigned int)));
+	else if (fms == 'c')
+		return (ft_putchar(va_arg(args, int)));
+	else if (fms == 's')
+		return (ft_putstr(va_arg(args, char *)));
+	else if (fms == 'p')
+		return (ft_putaddress(va_arg(args, void *)));
+	else if (fms == 'x' || fms == 'X')
+		return (ft_puthexa(va_arg(args, unsigned int), fms));
+	else if (fms == '%')
+		return (ft_putchar('%'));
+	return (0);
+}
+
 int	ft_printf(const char *fms, ...)
 {
 	va_list	args;
@@ -16,20 +35,8 @@ int	ft_printf(const char *fms, ...)
 		else
 		{
 			i++;
-			if (fms[i] == 'd' || fms[i] == 'i')
-				count += ft_putnbr(va_arg(args, int));
-			else if (fms[i] == 'u')
-				count += ft_putunbr(va_arg(args, unsigned int));
-			else if (fms[i] == 'c')
-				count += ft_putchar(va_arg(args, int));
-			else if (fms[i] == 's')
-				count += ft_putstr(va_arg(args, char *));
-			else if (fms[i] == 'p')
-				count += ft_putaddress(va_arg(args, void *));
-			else if (fms[i] == 'x' || fms[i] == 'X')
-				count += ft_puthexa(va_arg(args, unsigned int), fms[i]);
-			else if (fms[i] == '%')
-				count += ft_putchar('%');
+			if (fms[i])
+				count += ft_switch(args, fms[i]);
 		}
 		i++;
 	}
